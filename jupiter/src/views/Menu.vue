2e765aea815dd4a7087a420934774ef06584daa7
Mateo Rumac <85076814+mateorumac@users.noreply.github.com>
2024-04-20 14:40:48 +0200

    Delete jupiter directory

--- jupiter/src/views/Menu.vue
+++
@@ -1,9 +0,0 @@
-<template>
-    <section class="menu">
-      <div class="container">
-        <h2>Our Menu</h2>
-        <!-- Add menu items here -->
-      </div>
-    </section>
-  </template>
-  
